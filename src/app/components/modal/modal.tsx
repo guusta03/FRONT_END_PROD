3b893dbd64ad22251f5885776e81059c0dd474b1
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-16 17:07:10 -0300

    corrigi erro de digitação

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -26,7 +26,7 @@ export default function Modal({ state, closeModal, word }: ModalProps) {
   );
 
   const { data, loading } = useAxios<{ body: string }>({
-    url: `http://https://silly-tan-jersey.cyclic.app/api/meaning/word/${word}`,
+    url: `https://silly-tan-jersey.cyclic.app/api/meaning/word/${word}`,
     method: 'GET',
   });
 
