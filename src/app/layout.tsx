e7786b35ac574b7a9bcabf6d8b9b03ef3dcc0a46
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-20 18:40:55 -0300

    adciona Toaster provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,29 @@
-import { Metadata } from 'next';
-import * as React from 'react';
+'use client';
+
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { RecoilRoot } from 'recoil';
 
 import '@/styles/globals.css';
 import '@/styles/colors.css';
 
 import Header from '@/app/components/header/header';
-import { siteConfig } from '@/constant/config';
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  robots: { index: true, follow: true },
-  icons: {
-    icon: '/favicon/icons.png',
-  },
-  manifest: `/favicon/site.webmanifest`,
-  openGraph: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    type: 'website',
-    locale: 'en_US',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
-  },
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // const AppTheme = useRecoilValue(theme);
+
   return (
-    <html className='dark'>
-      <body>
-        <Header>{children}</Header>
-      </body>
-    </html>
+    <RecoilRoot>
+      <html>
+        <body>
+          <Toaster reverseOrder={false} gutter={8} position='top-right' />
+          <Header>{children}</Header>
+        </body>
+      </html>
+    </RecoilRoot>
   );
 }
